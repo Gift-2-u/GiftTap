a94c494fd6da76e2201b8a0b1f7fb22cf1dab740
Sébastien Latour <c.latour@hotmail.com>
2026-02-09 23:19:18 -0500

    fix: use correct secret variables for supabase

--- src/components/TapGame/supabase/functions/create-user-wallet/index.ts
+++ src/components/TapGame/supabase/functions/create-user-wallet/index.ts
@@ -3,7 +3,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.7"
 // Use this specific import - it's faster for the CPU to parse
 import * as tweetnacl from "https://esm.sh/tweetnacl@1.0.3"
 import { encode as encodeBase64 } from "https://deno.land/std@0.168.0/encoding/base64.ts"
-import { encode as encodeBase58 } from "https://deno.land/x/base58@v1.0.2/mod.ts"
+import { b58 } from "https://deno.land/x/b58@v1.0.2/mod.ts"
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
