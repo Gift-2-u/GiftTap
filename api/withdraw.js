ac429ee43606e475102b51d6de4f63e24c85fe69
Sébastien Latour <c.latour@hotmail.com>
2026-02-25 21:47:17 -0500

    redeploy

--- api/withdraw.js
+++ api/withdraw.js
@@ -7,6 +7,8 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  / This will list every variable name Vercel has loaded
+  console.log("Available Keys:", Object.keys(process.env));
 
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
