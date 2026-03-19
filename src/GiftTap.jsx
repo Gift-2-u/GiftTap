fd9694e307c5255ba85a00e0ece115da5dec65bd
Sébastien Latour <c.latour@hotmail.com>
2026-03-18 21:06:33 -0400

    W

--- src/GiftTap.jsx
+++ src/GiftTap.jsx
@@ -7,7 +7,7 @@ import Marketplace from './Marketplace';
 import Tasks from './Tasks';
 import Friends from './Friends';
 import Menu from './Menu';
-import whitepaperModal from './whitepaperModal';
+import WhitepaperModal from './WhitepaperModal';
 import bs58 from "bs58";
 import * as bip39 from 'bip39';
 import { derivePath } from 'ed25519-hd-key';
